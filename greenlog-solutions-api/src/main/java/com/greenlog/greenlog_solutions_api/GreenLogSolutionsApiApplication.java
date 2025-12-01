efe89bfe18fefb3c34471a62c665b2422c3649d6
Valadares24 <valadareslf0@gmail.com>
2025-12-01 15:17:22 -0300

    start projeto

---
+++ greenlog-solutions-api/src/main/java/com/greenlog/greenlog_solutions_api/GreenLogSolutionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.greenlog.greenlog_solutions_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenLogSolutionsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenLogSolutionsApiApplication.class, args);
+	}
+
+}
---
+++ greenlog-solutions-api/src/test/java/com/greenlog/greenlog_solutions_api/GreenLogSolutionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenlog.greenlog_solutions_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenLogSolutionsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
